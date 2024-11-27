963182b03ec75a434e9ea97a22986b2421e8d1de
Shin Morisawa <107992426+shinmorisawa@users.noreply.github.com>
2024-11-27 19:26:49 +0900

    Update decompress.py

--- src/decompress.py
+++ src/decompress.py
@@ -7,5 +7,3 @@ def extract_night(file_path, output_dir):
         with dctx.stream_reader(compressed_file) as decompressed:
             with tarfile.open(fileobj=decompressed, mode='r:') as tar:
                 tar.extractall(path=output_dir)
-
-extract_night('/home/shin/nightlinux/night/test/cool.tar.zst', '/home/shin/nightlinux/night/test')
